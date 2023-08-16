ce380b8f559abf9b1ea7f08400047b0be66b3500
Animesh <69345507+n-ce@users.noreply.github.com>
2023-08-16 21:13:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,23 @@
 const http = require('node:http');
 const fs = require('node:fs');
-
+const port = 8080;
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    const indexHtml = fs.readFileSync('./index.html', 'utf8');
+  if (req.url === '/' || req.url === 'index.html') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(indexHtml);
+    res.end(fs.readFileSync('./index.html', 'utf8'));
   }
   else if (req.url === '/style.css') {
-
-    const style = fs.readFileSync('./style.css');
     res.writeHead(200, { 'Content-Type': 'text/css' });
-    res.end(style);
+    res.end(fs.readFileSync('./style.css'));
   }
   else if (req.url === '/script.js') {
-    const js = fs.readFileSync('./script.js');
     res.writeHead(200, { 'Content-Type': 'application/javascript' });
-    res.end(js);
-
+    res.end(fs.readFileSync('./script.js'));
   }
-
   else {
     res.writeHead(404);
     res.end("error");
   }
 });
-
-server.listen(8080);
-console.log('Server started on port 8080');
+server.listen(port);
+console.log(`Server started on http://localhost:${port} .`);
